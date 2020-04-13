059d874a3aed9077485bafc644552f9bc4421c95
boleynen <58037455+boleynen@users.noreply.github.com>
2020-04-13 16:48:44 +0200

    added css for login & registratie

--- login.php
+++ login.php
@@ -1,11 +1,36 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css"/>
     <title>Login</title>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700;800&family=Source+Sans+Pro:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet">
 </head>
+
 <body>
-    
+
+    <form action="" method="post">
+
+        <div class="input-form">
+            <label for="email">Email</label>
+            <input type="text" id="email" name="email">
+        </div>
+
+        <div class="input-form">
+            <label for="wachtwoord">Wachtwoord</label>
+            <input type="text" id="wachtwoord" name="wachtwoord">
+        </div>
+
+        <div class="input-form submit-btn">
+            <input type="submit" id="submit-btn" value="AANMELDEN">
+        </div>
+
+    </form>
+
+    <a href="registratie.php" id="redirect-registratie">Heb je nog geen account? Registreer hier.</a>
+
 </body>
+
 </html>
--- registratie.php
+++ registratie.php
@@ -3,9 +3,35 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css"/>
     <title>Registratie</title>
 </head>
 <body>
-    
+
+    <form action="" method="post">
+
+        <div class="input-form">
+            <label for="email">Email</label>
+            <input type="text" id="email" name="email">
+        </div>
+
+        <div class="input-form">
+            <label for="wachtwoord">Wachtwoord</label>
+            <input type="text" id="wachtwoord" name="wachtwoord">
+        </div>
+
+        <div class="input-form">
+            <label for="wachtwoord-bevestigen">Bevestig je wachtwoord</label>
+            <input type="text" id="wachtwoord-bevestigen" name="wachtwoord-bevestigen">
+        </div>
+
+        <div class="input-form submit-btn">
+            <input type="submit" id="submit-btn" value="REGISTREREN">
+        </div>
+
+    </form>
+
+    <a href="login.php" id="redirect-login">Heb je al een account? Log hier in.</a>
+
 </body>
 </html>
